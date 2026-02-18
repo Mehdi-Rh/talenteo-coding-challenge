3fcb59db891b4c3079c6041a6a0408c53bf53331
Mehdi Rahal <mehdi@lablabee.com>
2026-02-18 11:48:39 +0100

    Add routing and Employees page; update sidebar navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import { DocumentsPage } from '@/pages/documents.page'
+
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { DocumentsPage } from '@/pages/documents.page';
+import { EmployeesPage } from '@/pages/employees.page';
 
 function App() {
-  return <DocumentsPage />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/employees" element={<EmployeesPage />} />
+        <Route path="/" element={<DocumentsPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/employees.page.tsx
@@ -0,0 +1,8 @@
+export function EmployeesPage() {
+    return (
+        <div>
+            <h1>Employees</h1>
+            {/* Employee table will go here */}
+        </div>
+    );
+}
